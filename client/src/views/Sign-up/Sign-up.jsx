ec272f5e675deebe960677b0361e36e78f4cd67c
ElissonXD <elissondias2006@gmail.com>
2026-02-24 17:54:13 -0300

    fix: update Sign-up.jsx

--- client/src/views/Sign-up/Sign-up.jsx
+++ client/src/views/Sign-up/Sign-up.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"
 import axios from "axios"
 import toast from "react-hot-toast"
 import { UserContext } from "../../contexts/UserContext"
-const apiUrl = import.meta.env.API_URL;
+const apiUrl = import.meta.env.VITE_API_URL;
 
 function SignUp() {
 
